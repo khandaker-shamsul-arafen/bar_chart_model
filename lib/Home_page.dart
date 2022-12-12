db6358f1b1e4cccfe2904bae27cf0ad5396e8da0
khandaker shamsul Arafen <khandakershamsul@gmail.com>
2022-12-12 16:26:13 +0600

    Bar chart

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -19,65 +19,60 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('D\'Chart')),
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(5),
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+            // child: DChartBarCustom(
+            //  // showMeasureLabel:true ,
+            //   spaceMeasureLinetoChart: 10.5,
+            //   showDomainLine: true,
+            //   showMeasureLine: true,
+            //   showDomainLabel: true,
+            //   showMeasureLabel: true,
+            //   spaceBetweenItem: 5,
+            //
+            //   listData: [
+            //     DChartBarDataCustom(
+            //      // showValue: true,
+            //       value: 13,
+            //       label: 'Jan',
+            //      // color: Colors.blue,
+            //     ),
+            //     DChartBarDataCustom(value: 20, label: 'Feb'),
+            //     DChartBarDataCustom(value: 30, label: 'Mar'),
+            //     DChartBarDataCustom(value: 40, label: 'Apr'),
+            //     DChartBarDataCustom(value: 25, label: 'Mei'),
+            //   ],
+            //
+            // ),
+            child: DChartBar(
+              data: const [
+                {
+                  'id': 'Bar',
+                  'data': [
+                    {'domain': '2020', 'measure': 3},
+                    {'domain': '2021', 'measure': 4},
+                    {'domain': '2022', 'measure': 6},
+                    {'domain': '2023', 'measure': 0.3},
+                    {'domain': '2024', 'measure': 10},
 
-            children: [
-              ListTile(
-                title: Text('Bar Chart'),
-                tileColor: Colors.green[200],
-              ),
-              Padding(
-                padding: EdgeInsets.all(16),
-                child: AspectRatio(
-                  aspectRatio: 16 / 9,
-                  // child: DChartBarCustom(
-                  //  // showMeasureLabel:true ,
-                  //   spaceMeasureLinetoChart: 10.5,
-                  //   showDomainLine: true,
-                  //   showMeasureLine: true,
-                  //   showDomainLabel: true,
-                  //   showMeasureLabel: true,
-                  //   spaceBetweenItem: 5,
-                  //
-                  //   listData: [
-                  //     DChartBarDataCustom(
-                  //      // showValue: true,
-                  //       value: 13,
-                  //       label: 'Jan',
-                  //      // color: Colors.blue,
-                  //     ),
-                  //     DChartBarDataCustom(value: 20, label: 'Feb'),
-                  //     DChartBarDataCustom(value: 30, label: 'Mar'),
-                  //     DChartBarDataCustom(value: 40, label: 'Apr'),
-                  //     DChartBarDataCustom(value: 25, label: 'Mei'),
-                  //   ],
-                  //
-                  // ),
-                  child: DChartBar(
-                    data: const [
-                      {
-                        'id': 'Bar',
-                        'data': [
-                          {'domain': '2020', 'measure': 3},
-                          {'domain': '2021', 'measure': 4},
-                          {'domain': '2022', 'measure': 6},
-                          {'domain': '2023', 'measure': 0.3},
-                        ],
-                      },
-                    ],
-                    domainLabelPaddingToAxisLine: 16,
-                    axisLineTick: 2,
-                   // axisLinePointTick: 2,
-                    axisLinePointWidth: 10,
-                    axisLineColor: Colors.green,
-                    measureLabelPaddingToAxisLine: 16,
-                    barColor: (barData, index, id) => Colors.amber,
-                    showBarValue: true,
-                  ),
-                ),
-              ),
-            ]
+
+                  ],
+                },
+              ],
+
+              domainLabelPaddingToAxisLine: 16,
+              axisLineTick: 2,
+              axisLinePointTick: 2,
+              axisLinePointWidth: 10,
+              axisLineColor: Colors.green,
+              measureLabelPaddingToAxisLine: 16,
+              barColor: (barData, index, id) => Colors.grey,
+              showBarValue: true,
+            ),
+          ),
         ),
       ),
     );
